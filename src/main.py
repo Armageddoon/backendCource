fb064254036f8e53fa17d9701540e9b600693572
alex <bolshuhin@list.ru>
2025-05-06 13:54:24 +0000

    Add users (model add unique email)

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-
-
-from src.config import settings
+from src.api.auth import router as router_auth
 
 app = FastAPI(docs_url=None)
 
-app.include_router(router_hotels, tags=["Отели"])
+app.include_router(router_auth)
+app.include_router(router_hotels)
+
 
 @app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
