3a1ed5646f73168d8c5810878e33dfaa5d541621
bolshuhin <bolshuhin@list.ru>
2025-05-07 04:30:38 +0000

    delete cookies

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, Request
 
+from src.api.dependencies import UserIdDep
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
 from src.schemas.users import UserRequestAdd, UserAdd
@@ -28,20 +29,24 @@ async def login_user(
 ):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
-        print(user)
-        if not user or user is None:
+        if not user:
             raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
         if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-
         access_token = AuthService().create_access_token({"user_id": user.id})
         response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
 
 
-@router.get("/only_auth")
-async def only_auth(
-        request: Request,
+@router.get("/me")
+async def get_me(
+        user_id: UserIdDep,
 ):
-    access_token = request.cookies['access_token']
-    return access_token
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+        return user
+
+@router.post("/logout")
+async def log_out(response: Response) -> None:
+    response.delete_cookie("access_token")
+    return {"message": "Logged out successfully"}
