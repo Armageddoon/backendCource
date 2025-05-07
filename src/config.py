f07f070b6deb9d33eb34b60c8c6efd8be250e93c
bolshuhin <bolshuhin@list.ru>
2025-05-07 02:23:01 +0000

    Autentification

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
@@ -13,6 +14,10 @@ class Settings(BaseSettings):
     @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     model_config = SettingsConfigDict(env_file=".env")
 
--- src/database.py
+++ src/database.py
@@ -1,11 +1,9 @@
-from sqlalchemy.ext import asyncio
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import text
 
 from src.config import settings
 
-engine = create_async_engine(settings.DB_URL, echo=True)
+engine = create_async_engine(settings.DB_URL)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
